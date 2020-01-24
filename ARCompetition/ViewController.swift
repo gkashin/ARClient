3b1106ecb853b550d8e3230ed6abde3fce3901bd
George <georgii.kashin@gmail.com>
2020-01-24 20:42:28 +0300

    Initial Commit

---
+++ ARCompetition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARCompetition
+//
+//  Created by Георгий Кашин on 24.01.2020.
+//  Copyright © 2020 Georgii Kashin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
